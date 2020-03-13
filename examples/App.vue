3f1bd40b12bb0031b614c23245373fa184f0f927
邱基荣 <qiujirong@wtoip.com>
2020-03-13 19:57:21 +0800

    fix: config

---
+++ examples/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <div class="page-container">app</div>
+  </div>
+</template>
+<style lang="scss">
+html, body, #app {
+  height: 100%;
+}
+
+.page-container {
+  height: 400px;
+}
+</style>
